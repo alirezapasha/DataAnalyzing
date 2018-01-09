e47ef862365d577d13e05ff1dcdab46cd111d6a4
Alireza Pasha <alireza@alirezaPasha.lan>
2018-01-09 02:00:53 -0800

    add exception to the code

--- lastweekdec/lessonone.py
+++ lastweekdec/lessonone.py
@@ -3,4 +3,27 @@ Created on Dec 30, 2017
 
 @author: alireza
 '''
-print "it is started"
+#import packages
+import csv
+from pandas.compat import FileNotFoundError
+
+
+#declare constants
+DATABASE_PATH = '/home/Alirezda/Work/DataBase/bitcoin_one_year.csv' 
+count = 0
+
+#loading a file
+try:    
+    with open(DATABASE_PATH, 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        
+        for line in csv_reader :
+            print line
+            count += 1
+         
+    print count
+    
+except FileNotFoundError as e:
+    print "an Exception happen during get the file ", e
+except Exception:
+    print "something wrongs"
--- lastweekdec/secondlesson.py
+++ lastweekdec/secondlesson.py
@@ -3,4 +3,9 @@ Created on Dec 30, 2017
 
 @author: alireza
 '''
-print("just checking github commit")
+import math
+li = [-10, 2, -3, 4, -5, 9]
+si = sorted(li, key=abs)
+li.sort(reverse=True)
+print li
+print si
