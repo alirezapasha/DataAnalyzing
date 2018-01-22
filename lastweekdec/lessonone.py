da577f11cc6a1e2b32efade6784c1f538d93f4d1
Alireza Pasha <alireza@alirezaPasha.lan>
2018-01-21 20:50:25 -0800

    lesson 4 mockup to article done!

--- lastweekdec/lessonone.py
+++ lastweekdec/lessonone.py
@@ -9,7 +9,7 @@ from pandas.compat import FileNotFoundError
 
 
 #declare constants
-DATABASE_PATH = '/home/Alirezda/Work/DataBase/bitcoin_one_year.csv' 
+DATABASE_PATH = '/home/Alireza/Work/DataBase/bitcoin_one_year.csv' 
 count = 0
 
 #loading a file
@@ -18,7 +18,7 @@ try:
         csv_reader = csv.reader(csv_file)
         
         for line in csv_reader :
-            print line
+#             print line
             count += 1
          
     print count
@@ -27,3 +27,6 @@ except FileNotFoundError as e:
     print "an Exception happen during get the file ", e
 except Exception:
     print "something wrongs"
+    
+def add(a, b):
+    return a+b
